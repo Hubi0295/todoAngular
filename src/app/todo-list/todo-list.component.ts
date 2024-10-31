1b552c3869b110c4c40f3d46192ee00f2c56d030
= <nowa nazwa>
2024-11-01 00:18:59 +0100

    Zapoznanie z component livecycle hooks

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ViewChild, ViewChildren } from '@angular/core';
 import { Todo } from '../shared/interfaces/todo.interface';
+import { TodoComponent } from './todo/todo.component';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements AfterViewChecked {
     todos: Todo[]=[];
     errorMessage ='';
+    @ViewChild(TodoComponent) TodoC !: TodoComponent;
+    @ViewChildren(TodoComponent) TodoCs !: TodoComponent;
+    ngAfterViewInit(): void{
+        console.log(this.TodoC);
+    }
+    ngAfterViewChecked(): void {
+        console.log(this.TodoC);
+    }
     addTodo(todo: string): void {
       if(todo.length>3){
         this.todos.push({name: todo, isComplete: false});
@@ -26,5 +35,11 @@ export class TodoListComponent {
 
   deleteTodo(i: number) {
     this.todos = this.todos.filter((todo,index)=> index!=i)
+  }
+  changeTodoStatus(i: number){
+      this.todos[i]={
+        ...this.todos[i],
+        isComplete : !this.todos[i].isComplete
+      }
   }
 }
--- src/app/todo-list/todo/todo.component.ts
+++ src/app/todo-list/todo/todo.component.ts
@@ -1,17 +1,43 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {AfterViewInit, Component, DoCheck, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {Todo} from '../../shared/interfaces/todo.interface'
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnChanges, OnInit, DoCheck, AfterViewInit,OnDestroy {
+
     @Input() task!: Todo;
     @Input() i!: number;
     @Output() delete = new EventEmitter<void>();
+    @Output() changeStatus = new EventEmitter<number>();
+    @ViewChild('li') li!:ElementRef;
+    
     openModal = false;
-    changeTodoStatus(todo: Todo){
-      todo.isComplete = !todo.isComplete;
+    timeout!: number;
+    changeTodoStatus(){
+      this.changeStatus.emit(this.i);
+  }
+  constructor(){
+    console.log(this.task);
+  }
+  ngOnDestroy(): void {
+      console.log('destroy')
+      clearTimeout(this.timeout);
+  }
+  ngAfterViewInit(): void {
+      console.log(this.li);
+  }
+  ngDoCheck(): void {
+      console.log('NG DOCHECK DZIALA');
+  }
+  ngOnInit(): void {
+    this.timeout = setTimeout(()=>{
+      console.log('SetTimeout');
+    },3000)
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+        console.log(changes);
   }
   toggleModal(): void{
     this.openModal=!this.openModal;
